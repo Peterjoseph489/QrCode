31a36e8b3d86b4a15bdd419c3e7ba81070a7a5fa
Joseph <josephpeter489@gmail.com>
2023-09-29 10:52:47 +0100

    Almost there

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 require('./configDB/userDB');
+const userRoute = require('./routes/users.route');
+
+require('dotenv').config();
 const express = require('express');
+const exphbs = require('nodemailer-express-handlebars');
+const fileUpload = require('express-fileupload');
 const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use("/api", userRoute)
+
+app.use(fileUpload({
+  useTempFiles: true
+}));
+app.set('view engine', 'handlebars');
 
 app.listen(PORT, ()=>{
     console.log(`This server is connected on Port: ${PORT}`)
